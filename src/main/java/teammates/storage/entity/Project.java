44c2ab095d11af515084aa3ad43548861618d021
mjh2g3 <48746074+mjh2g3@users.noreply.github.com>
2021-05-23 22:08:26 -0700

    Create Project.java
    This entities class is required for the EntitiesDb and ProjectsDb as a <E entity, A attributes> pairing. The entity would be Project. When retrieving and creating entities, will need an entity object to send to the google data store

---
+++ src/main/java/teammates/storage/entity/Project.java
@@ -0,0 +1,23 @@
+package teammates.storage.entity;
+
+import java.time.Instant;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+import com.googlecode.objectify.annotation.Translate;
+
+import teammates.common.util.Const;
+
+/**
+ *
+ * Project entity definition
+ */
+public class Project extends BaseEntity {
+    //variables
+  
+    //constructor
+    //getters
+    //setters
+    //copy paste of ProjectAttributes?
+}
