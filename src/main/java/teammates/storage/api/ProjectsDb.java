3a62ed9510fe50356d2569c5a295c646f9b7178c
mjh2g3 <48746074+mjh2g3@users.noreply.github.com>
2021-05-28 17:52:39 -0700

    added new db and entity code
    added db and entity

--- src/main/java/teammates/storage/api/ProjectsDb.java
+++ src/main/java/teammates/storage/api/ProjectsDb.java
@@ -1,26 +1,27 @@
-
 package teammates.storage.api;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+import com.google.appengine.api.search.Results;
+import com.google.appengine.api.search.ScoredDocument;
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.cmd.Query;
+
 import java.time.Instant;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//Import CSV/file database writer/reader
-
+import teammates.common.util.Logger;
 import com.googlecode.objectify.cmd.LoadType;
 import teammates.common.datatransfer.attributes.ProjectAttributes;
 import teammates.storage.entity.Project;
 import teammates.common.util.Assumption;
+import teammates.common.util.JsonUtils;
 import teammates.common.exception.EntityDoesNotExistException;
 import teammates.common.exception.InvalidParametersException;
+import teammates.common.exception.EntityAlreadyExistsException;
 
 /**
  *
@@ -28,37 +29,45 @@ import teammates.common.exception.InvalidParametersException;
  *
  */
 public class ProjectsDb extends EntitiesDb<Project, ProjectAttributes> {
-  //no parameters
+    
+    private static final Logger log = Logger.getLogger();
 
-    // TODO finish implementing this method
+    @Override
+    public ProjectAttributes createEntity(ProjectAttributes project)
+        throws InvalidParametersException, EntityAlreadyExistsException {
+        
+	    ProjectAttributes createdProject = super.createEntity(project);
+        log.info("Entity created: " + JsonUtils.toJson(createdProject));
+	    return createdProject;
+    }
+    //getProject
+    //updateProject
+    //deleteProject
+
+
+    // 
     @Override
     public boolean hasExistingEntities(ProjectAttributes entityToCreate)
     {
-        return false;
+        return !load()
+                .filterKey(Key.create(Project.class,
+                        Project.generateId(entityToCreate.getProjectName(), entityToCreate.getCourseId())))
+                .list()
+                .isEmpty();
     }
 
-    // TODO finish implementing this method
+    //
     @Override
     LoadType<Project> load() {
-        return null;
+        return ofy().load().type(Project.class);
     }
 
-    // TODO finish implementing this method
+    // 
     @Override
     ProjectAttributes makeAttributes(Project entity) {
-        return null;
+        Assumption.assertNotNull(entity);
+
+        return ProjectAttributes.valueOf(entity);
     }
-
-  
-  //createProject
-    //see the EntitiesDb class for createEntity flow
-    //createEntity calls out for the *Attributes class to call a method
-    //.toEntity() which returns an entity E. If we reuse the super class
-    //method, then we would need to ensure our attributes class covers
-    //this functionality
-
-  //getProject
-  //updateProject
-  //deleteProject
-    
+      
 }
